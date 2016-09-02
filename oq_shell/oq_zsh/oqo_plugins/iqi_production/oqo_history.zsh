a88f6d71f844bd4cb0db2be498489f5c3ecf422b
indieterminacy <indieterminacy@nortonspeal.com>
2016-09-02 16:51:08 +0200

    Random commit after duplicating the plugin

---
+++ oq_shell/oq_zsh/oqo_plugins/iqi_production/oqo_history.zsh
@@ -0,0 +1,28 @@
+#!/usr/bin/zsh
+
+#https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/history
+#
+# Save dirstack history to .zdirs
+# adapted from:
+# github.com/grml/grml-etc-core/blob/master/etc/zsh/zshrc#L1547
+#https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/dirpersist
+#
+#
+#
+# Zsh plugin giving you a history which is sensitive to the directory you are
+# currently in
+# Not sure if functional
+# zplug "tymm/zsh-directory-history"
+#  # Bind up/down arrow keys to navigate through your history
+#  bindkey '\e[A' directory-history-search-backward
+#  bindkey '\e[B' directory-history-search-forward
+#
+#  # Bind CTRL+k and CTRL+j to substring search
+#  bindkey '^j' history-substring-search-up
+#  bindkey '^k' history-substring-search-down
+#
+
+# Per directory history for zsh, as well as global history, and the ability to
+# toggle between them with ^G.
+# Not sure if functional
+# zplug "jimhester/per-directory-history"
